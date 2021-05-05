01d78a07f4dde35fe14d2ffb5a92b6158a432ea7
deoez <deoadzar89@gmail.com>
2021-05-05 11:56:57 +0700

    SplashScreen check session

--- app/src/main/java/com/example/madhang_ae/Pembeli/MakananFragment.java
+++ app/src/main/java/com/example/madhang_ae/Pembeli/MakananFragment.java
@@ -2,65 +2,42 @@ package com.example.madhang_ae.Pembeli;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.example.madhang_ae.R;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MakananFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MakananFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public MakananFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MakananFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MakananFragment newInstance(String param1, String param2) {
-        MakananFragment fragment = new MakananFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    BottomSheetBehavior bottomSheetBehaviorMakanan;
+    LinearLayout linearLayoutBsMakanan;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_makanan, container, false);
+        View v = inflater.inflate(R.layout.fragment_makanan, container, false);
+        linearLayoutBsMakanan = v.findViewById(R.id.bottomSheetMakanan);
+        bottomSheetBehaviorMakanan = BottomSheetBehavior.from(linearLayoutBsMakanan);
+        bottomSheetBehaviorMakanan.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                if (newState == BottomSheetBehavior.STATE_EXPANDED) {
+
+                } else if (newState == BottomSheetBehavior.STATE_COLLAPSED) {
+
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+            }
+        });
+        return v;
     }
 }
