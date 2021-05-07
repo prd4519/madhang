20753290f2a7edd55fef6db562a52c8462e99282
deoez <deoadzar89@gmail.com>
2021-05-07 09:04:58 +0700

    bottomsheet makanan clear

--- app/src/main/java/com/example/madhang_ae/API/UtilsApi.java
+++ app/src/main/java/com/example/madhang_ae/API/UtilsApi.java
@@ -2,8 +2,8 @@ package com.example.madhang_ae.API;
 
 public class UtilsApi {
 
-   public static final String BASE_URL = "http://172.23.200.44/Madhang_ae/";
-    public static final String IMAGE_URL = "http://172.23.200.44/Madhang_ae/assets/files/image/";
+   public static final String BASE_URL = "http://172.23.200.47/Madhang_ae/";
+    public static final String IMAGE_URL = "http://172.23.200.47/Madhang_ae/assets/files/image/";
 
     public static BaseApiService getApiService(){
         return RetrofitClient.getClient(BASE_URL).create(BaseApiService.class);
