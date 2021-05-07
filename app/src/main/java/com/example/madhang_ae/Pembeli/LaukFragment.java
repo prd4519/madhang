a7489aa1e5a13733c101704f014782b395593d47
prd4519 <pradhanard4519@gmail.com>
2021-05-07 15:07:27 +0700

    Button Float

--- app/src/main/java/com/example/madhang_ae/Pembeli/LaukFragment.java
+++ app/src/main/java/com/example/madhang_ae/Pembeli/LaukFragment.java
@@ -1,5 +1,9 @@
 package com.example.madhang_ae.Pembeli;
 
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,60 +11,70 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.madhang_ae.EditProfile;
+import com.example.madhang_ae.Penjual.NavigationPenjual;
 import com.example.madhang_ae.R;
+import com.example.madhang_ae.SessionManager;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LaukFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class LaukFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public LaukFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LaukFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LaukFragment newInstance(String param1, String param2) {
-        LaukFragment fragment = new LaukFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    AlertDialog.Builder dialogBuilder;
+    AlertDialog alertDialog;
+    SessionManager sessionManager;
+    CircleImageView fabPop;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lauk, container, false);
+        View v =  inflater.inflate(R.layout.fragment_lauk, container, false);
+        fabPop = v.findViewById(R.id.popup2);
+        fabPop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPopUp2();
+            }
+        });
+
+        return v;
+    }
+
+    private void showPopUp2(){
+        dialogBuilder = new AlertDialog.Builder(getActivity());
+        View layoutView = getLayoutInflater().inflate(R.layout.popup_pembeli, null);
+        Button edtProfile = layoutView.findViewById(R.id.edtProfile);
+        Button dashboardpnjl = layoutView.findViewById(R.id.dshbrPenjual);
+        Button logout = layoutView.findViewById(R.id.Logout);
+        dialogBuilder.setView(layoutView);
+        alertDialog = dialogBuilder.create();
+        alertDialog.setCancelable(true);
+        alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        alertDialog.show();
+        edtProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), EditProfile.class);
+                startActivity(intent);
+                getActivity().finish();
+                alertDialog.dismiss();
+            }
+        });
+        dashboardpnjl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), NavigationPenjual.class);
+                startActivity(intent);
+                getActivity().finish();
+                alertDialog.dismiss();
+            }
+        });
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sessionManager.logout();
+            }
+        });
     }
 }
